088d59063d8acefe98f6b412784e43ee9888e0c2
yamauchi.ryoji <yamauchi.ryoji@m-field.co.jp>
2018-11-08 18:31:07 +0900

    styleのテスト

--- test/counter.spec.js
+++ test/counter.spec.js
@@ -13,16 +13,43 @@ describe('Counter', () => {
     expect(wrapper.vm.count).toBe(0)
   })
 
-  it ('increments the count the button is clicked', () => {
+  it ('increments the count the increment button is clicked', () => {
     // 最初は0だけれども
     expect(wrapper.vm.count).toBe(0)
 
     // ボタンをおすと
-    wrapper.find('button').trigger('click');
+    wrapper.find('.increment').trigger('click')
 
     // 1になる
     expect(wrapper.vm.count).toBe(1)
   })
+
+  it ('decrement the count the decrement button is clicked', () => {
+
+    wrapper.setData({
+      count :5
+    })
+
+    wrapper.find('.decrement').trigger('click')
+
+    expect(wrapper.vm.count).toBe(4)
+  })
+
+  // onlyをつけると、onlyがある箇所のみテストをしてくれる！
+  //it.only ('never goes below 0', () => {
+  it ('never goes below 0', () => {
+    expect(wrapper.vm.count).toBe(0)
+
+    expect(wrapper.find('.decrement').hasStyle('display', 'none')).toBe(true)
+    //wrapper.find('.decrement').trigger('click');
+    //expect(wrapper.vm.count).toBe(0)
+
+    wrapper.find('.increment').trigger('click')
+
+    expect(wrapper.find('.decrement').hasStyle('display', 'none')).toBe(false)
+  })
+
+
 
   it ('presents the current count', () => {
     expect(wrapper.find('.count').html()).toContain(0)
