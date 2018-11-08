f0f87aad26636f1367b10cb7eee5b91914aa7652
yamauchi.ryoji <yamauchi.ryoji@m-field.co.jp>
2018-11-08 18:12:23 +0900

    beforeEachを使用

--- test/counter.spec.js
+++ test/counter.spec.js
@@ -3,15 +3,17 @@ import Counter from '../src/components/Counter.vue'
 import expect from 'expect'
 
 describe('Counter', () => {
+  let wrapper
 
+  beforeEach(() => {
+    wrapper = mount(Counter)
+  })
 
   it ('default to a count of 0', () => {
-    let wrapper = mount(Counter)
     expect(wrapper.vm.count).toBe(0)
   })
 
   it ('increments the count the button is clicked', () => {
-    let wrapper = mount(Counter)
     // 最初は0だけれども
     expect(wrapper.vm.count).toBe(0)
 
@@ -23,7 +25,6 @@ describe('Counter', () => {
   })
 
   it ('presents the current count', () => {
-    let wrapper = mount(Counter)
     expect(wrapper.find('.count').html()).toContain(0)
   })
 
