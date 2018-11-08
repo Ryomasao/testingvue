2ec33d4f49a2a2ead4c7158b85c44b487910026a
yamauchi.ryoji <yamauchi.ryoji@m-field.co.jp>
2018-11-08 17:54:56 +0900

    first commit

---
+++ test/counter.spec.js
@@ -0,0 +1,30 @@
+import { mount } from 'vue-test-utils'
+import Counter from '../src/components/Counter.js'
+import expect from 'expect'
+
+describe('Counter', () => {
+
+
+  it ('default to a count of 0', () => {
+    let wrapper = mount(Counter)
+    expect(wrapper.vm.count).toBe(0)
+  })
+
+  it ('increments the count the button is clicked', () => {
+    let wrapper = mount(Counter)
+    // 最初は0だけれども
+    expect(wrapper.vm.count).toBe(0)
+
+    // ボタンをおすと
+    wrapper.find('button').trigger('click');
+
+    // 1になる
+    expect(wrapper.vm.count).toBe(1)
+  })
+
+  it ('presents the current count', () => {
+    let wrapper = mount(Counter)
+    expect(wrapper.find('.count').html()).toContain(0)
+  })
+
+})
