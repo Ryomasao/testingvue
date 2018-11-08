eb718b4c85432159648097ac06ef5d7a624a59a2
yamauchi.ryoji <yamauchi.ryoji@m-field.co.jp>
2018-11-08 18:08:29 +0900

    単一ファイルコンポーネントに置き換え

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,17 @@
+const VueLoaderPlugin = require('vue-loader/lib/plugin')
+
 module.exports = {
-  mode: 'development'
-}
+  mode: 'development',
+  module: {
+    rules: [
+      {
+        test: /\.vue$/,
+        use: 'vue-loader'
+      }
+    ]
+  },
+  plugins: [
+    // 動画で使っているvue-loaderより新しいvue-loaderはpluginの定義が必須みたい
+    new VueLoaderPlugin()
+  ]
+};
